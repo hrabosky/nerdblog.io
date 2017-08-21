4dc5ebaa5d8f0146a5f5bcc5f4e66c6ded2b4610
hrabosky <hraboskyjr@gmail.com>
2017-08-20 19:01:15 -0500

    Collaborating with Claybeard to update server.js file, connected to the mongodb instance.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express')
 const app = express()
 
@@ -8,3 +10,17 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
 	console.log('Example app listening on port 3000!')
 })
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+ 
+// Connection URL 
+var url = 'mongodb://localhost:27017/nerdblog';
+// Use connect method to connect to the Server 
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+ 
+  db.close();
+});
+
