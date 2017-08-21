e6f59a794c10d6dae4d72cc401f5c8f7078ff814
hrabosky <hraboskyjr@gmail.com>
2017-08-20 19:31:03 -0500

    Added documents

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express')
 const app = express()
 
@@ -20,7 +18,28 @@ var url = 'mongodb://localhost:27017/nerdblog';
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server");
- 
+	insertDocuments(db, function(){
+	console.log('Finished inserting documents');
+}) 
   db.close();
 });
+
+
+
+//Inserting a Document
+
+var insertDocuments = function(db, callback)  {
+	// Get the documents collection
+	var collection = db.collection('documents');
+	// Insert some documents
+	collection.insertMany([
+		{a : 1}, {a : 2}, {a : 3}
+	], function(err, result) {
+	assert.equal(err, null);
+	assert.equal(3, result.result.n);
+	assert.equal(3, result.ops.length);
+	console.log("Inserted 3 documents into the document collection");
+	callback(result);
+	});
+}
 
